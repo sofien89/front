6d7b6839931c7dd6883db1ce5c350d930d8f6049
Sofien <sofienbouafia@gmail.com>
2022-03-30 12:53:52 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./assets/main.scss";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+reportWebVitals();
